7abc67e2e471e7c0a66f2614f310f1f7dbd15177
lxd <xdli@thoughtworks.com>
2014-01-28 17:17:45 +0800

    Add more scripts and ack

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ hyper-v-ps1-scripts
 ===================
 
 PowerShell scripts for Manage Windows Hyper-V Server 2012
+
+
+
+Acknowledgement
+---------------
+Some of the scripts come from:
+* http://technet.microsoft.com/en-us/magazine/jj933287.aspx
+* http://blogs.technet.com/b/ptsblog/archive/2011/12/09/extending-remote-desktop-services-using-powershell.aspx
+* http://pc-addicts.com/12-steps-to-remotely-manage-hyper-v-server-2012-core/
+
---
+++ new-hyper-v-machine-with-vhd.ps1
@@ -0,0 +1,30 @@
+param (
+    [string]$NAME = "TW-DEV1",
+    [long]$RAMSIZE = 4GB,
+    [long]$VHDSIZE = 80GB,
+    [string]$VMLOC = "C:\HyperV",
+    [string]$VHDPATH = "C:\HyperV\TW-DEV1.vhdx",
+    [string]$ExternalSwitch = "ExternalSwitch",
+    [string]$NetworkAdapter = "Ethernet",
+    [int]$CPUCount = 4
+)
+
+
+# Create VM Folder
+MD $VMLOC -ErrorAction SilentlyContinue
+
+# Create Network Switch
+$TestSwitch = Get-VMSwitch -Name $ExternalSwitch -ErrorAction SilentlyContinue; if ($TestSwitch.Count -EQ 0){New-VMSwitch -Name $ExternalSwitch -NetAdapterName $NetworkAdapter -EnableIov $True}
+
+# Create Virtual Machines
+New-VM -Name $NAME -Path $VMLOC -MemoryStartupBytes $RAMSIZE -VHDPath $VHDPATH -SwitchName $ExternalSwitch
+
+# Configure Virtual Machines
+Set-VMDvdDrive -VMName $NAME -Path $ISO
+Set-VMProcessor -VMName $NAME -Count $CPUCount
+
+# The following line only make graphics worse
+# Add-VMRemoteFx3dVideoAdapter -VMName $Name
+
+
+Start-VM $NAME
